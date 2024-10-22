1f7d42841858a5f0ca494279bebfdbdf6671017a
Ouarhim <younes.ouarhim@student-cs.fr>
2024-10-22 09:57:35 +0200

    renamed:    The Best/codes_hier_soir.ipynb -> The Best/compartmental_model.ipynb 	modified:   The Best/correlation.ipynb 	new file:   The Best/instagram_accounts.csv 	new file:   The Best/instagram_posts_0911_1111.csv 	new file:   The Best/instagram_posts_1211_1611.csv 	new file:   The Best/verification.py 	modified:   The Best/viralmarketinf_flux_thebest-2.ipynb

---
+++ The Best/verification.py
@@ -0,0 +1,16 @@
+## Question did everyone post ? 
+
+import pandas as pd 
+
+posts_1 = pd.read_csv("instagram_posts_0911_1111.csv", delimiter=',') 
+posts_2 = pd.read_csv("instagram_posts_1211_1611.csv", delimiter=',')
+
+posts = pd.concat([posts_1, posts_2],axis = 0)
+
+accounts = pd.read_csv("instagram_accounts.csv")
+
+# If no assertion error, then the answer is yes, furthermore the accounts and posts have the same length, meaning that every user posted once and only once.
+
+assert posts.shape[0] == accounts.shape[0], "The accounts and posts datasets have the same lengths"
+assert set(accounts['id_user']) - set(posts['id_user'])  == set(), "There are people who never posted"
+ 
